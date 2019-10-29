41c6cc6a593bd1fefce43368110709b50da039f2
Tony <tony.siu.17@ucl.ac.uk>
2019-10-29 19:42:16 +0000

    redefine BBI axioms and define a Structure datatype

--- BBI.thy
+++ BBI.thy
@@ -6,135 +6,86 @@ theory BBI
   imports Main
 begin
 
+text \<open> In this theory, we are defining syntax for Boolean BI formula and its rules and axioms.
+The logic system is syntax and without any semantic at this level\<close>
+
 section "BBI formula definiton"
 
-text \<open>First we define a variable which is indexed by natural number. 
-In this way we can genereate inifinte atomic proposition varaiables\<close>
+text \<open>Initially, we define a variable which is indexed by natural number. 
+In this way we can genereate/assuming  inifinte atomic proposition varaiables\<close>
 
 datatype atom = P nat
 
-text \<open>We now define a axiom which can evaluate atom to boolean without define how the function works
-We are generalising the method. This method allows Isabelle to cast an atomic variable to a boolean type\<close>
-
-axiomatization evalP :: "atom \<Rightarrow> bool" 
-
-text \<open>With inifinte proposition variables we can now define a BBI formula datatype.
+text \<open>BBI is CL + IL. In our definition we include both syntax in the definition.
+With inifinte proposition variables we can now define a BBI formula datatype.
 It is a recursive definition which includes symbols (both prefix and infix).
 The natural number next to the symbol indicate its precedence Isabell recommended range is >= 100\<close>
 
 datatype BBI_form = 
   Truth                     ("\<top>\<^sub>B")
   | Falsity                 ("\<bottom>\<^sub>B")
+  | Mfalsity                ("\<top>\<^sup>*\<^sub>B")
   | Atom atom
-  | Neg  BBI_form           ("\<not>\<^sub>B _" 100)
-  | Mneg BBI_form           ("~\<^sub>B _" 100)
+  | Neg BBI_form            ("\<not>\<^sub>B _" 100)
   | Con BBI_form BBI_form   (infix "\<and>\<^sub>B" 101)
   | MCon BBI_form BBI_form  (infix "*\<^sub>B" 101)
   | Dis BBI_form BBI_form   (infix "\<or>\<^sub>B" 101)
-  | Mdis BBI_form BBI_form  (infix "\<or>*\<^sub>B" 101)
   | Imp  BBI_form BBI_form  (infix "\<rightarrow>\<^sub>B" 101)
-  | Mimp  BBI_form BBI_form  (infix "\<rightarrow>*\<^sub>B" 101)
+  | Mimp  BBI_form BBI_form (infix "\<rightarrow>\<^emph>\<^sub>B" 101)
 
-text \<open>BBI_form is a custom mathematical object. Therefore it is difficult for Isabelle to prove any
-thing on this object. Therefore, we define a primitive recursive function to evaluate the formula to 
-boolean type\<close>
+text \<open>Now we define axioms and rules for BBI-formula.
+inductive  keyword allows us to formalise a more readable proof system in Isabelle.
+This is a common techique to define a Hilbert's style proof system as "=> bool isnt necessarily 
+a semantic meaning"
+In Isabelle, when defining an axioms, e.g.MP:  A -> B  ==> A  ==> B, it means that 
+Assume A implies B is true and A is true will prove B is true.
+Using this notation we can define a inductive proof system\<close>
 
-primrec evalF :: "BBI_form \<Rightarrow> bool" where
-  "evalF Truth = True"|
-  "evalF Falsity = False"|
-  "evalF (Atom p) = evalP p"|
-  "evalF (\<not>\<^sub>B \<phi>) = (\<not>(evalF \<phi>))"|
-  "evalF (~\<^sub>B \<phi>) =  (\<not>(evalF \<phi>))" |
-  "evalF (\<phi> \<and>\<^sub>B \<psi>) = ((evalF \<phi>)\<and>(evalF \<psi>))"|
-  "evalF (\<phi> *\<^sub>B \<psi>) = ((evalF \<phi>)\<and>(evalF \<psi>))"|
-  "evalF (\<phi> \<or>\<^sub>B \<psi>) = ((evalF \<phi>)\<or>(evalF \<psi>))"|
-  "evalF (\<phi> \<or>*\<^sub>B \<psi>) = ((evalF \<phi>)\<or>(evalF \<psi>))"|
-  "evalF (\<phi> \<rightarrow>\<^sub>B \<psi>) = ((evalF \<phi>)\<longrightarrow>(evalF \<psi>))"|
-  "evalF (\<phi> \<rightarrow>*\<^sub>B \<psi>) = ((evalF \<phi>)\<longrightarrow>(evalF \<psi>))"
+subsection "IL + CL Axioms"
 
+(* Initial attemp of turnstile *)
+inductive turnstile_CL :: "BBI_form \<Rightarrow> BBI_form \<Rightarrow> bool" (infix "\<turnstile>\<^sub>c" 55)
+  where 
+Ax : "F \<turnstile>\<^sub>c F"|
+Top : "F \<turnstile>\<^sub>c \<top>\<^sub>B"|
+Bot : "\<bottom>\<^sub>B \<turnstile>\<^sub>c F"|
+imp : "F \<and>\<^sub>B G \<turnstile>\<^sub>c H \<Longrightarrow> F \<turnstile>\<^sub>c G \<rightarrow>\<^sub>B H"|
+MP : "F \<turnstile>\<^sub>c G \<Longrightarrow> G \<turnstile>\<^sub>c H \<Longrightarrow> F \<turnstile>\<^sub>c H"|
+notl: "(\<not>\<^sub>B F) \<turnstile>\<^sub>c F \<rightarrow>\<^sub>B \<bottom>\<^sub>B" |
+notr: "F \<rightarrow>\<^sub>B \<bottom>\<^sub>B \<turnstile>\<^sub>c (\<not>\<^sub>B F)"|
+notnot : "(\<not>\<^sub>B \<not>\<^sub>B F) \<turnstile>\<^sub>c F" |
+ConjI : "F \<turnstile>\<^sub>c G \<Longrightarrow> F \<turnstile>\<^sub>c H \<Longrightarrow> F \<turnstile>\<^sub>c G \<and>\<^sub>B H" |
+DisjE : "F \<turnstile>\<^sub>c H \<Longrightarrow> G \<turnstile>\<^sub>c H \<Longrightarrow> F \<or>\<^sub>B G \<turnstile>\<^sub>c H"|
+ConjE1 : "F \<and>\<^sub>B H \<turnstile>\<^sub>c F"|
+ConjE2 : "F \<and>\<^sub>B H \<turnstile>\<^sub>c H" |
+DisjI1 : "F \<turnstile>\<^sub>c F \<and>\<^sub>B H" |
+DisjI2 : "H \<turnstile>\<^sub>c F \<and>\<^sub>B H"
 
-text \<open>The following evaluation is used to prove negation of atomic BBI formula can produce correct results
-As all BBI formula are borken down to a list of atomic propositions. Therefore, through a induction proof \<close>
+text \<open>In here, we define an extra symbole: double turnstile for future usage.
+The definition is based on the inductive definition of turnstile
+\<close>
 
-lemma negateAtom : 
-  "evalF (Neg (Atom a)) \<Longrightarrow>  \<not>(evalP a)" 
-  "evalF (Mneg (Atom a)) \<Longrightarrow>  \<not>(evalP a)"
-  "evalF (Neg Truth) \<Longrightarrow>  False" 
-  "evalF (Neg Falsity) \<Longrightarrow>  True" 
-  "evalF (Mneg Truth) \<Longrightarrow>  False" 
-  "evalF (Mneg Falsity) \<Longrightarrow> True" 
-  by auto
+definition double_turnstile_CL :: "BBI_form \<Rightarrow> BBI_form \<Rightarrow> bool" (infix "\<stileturn>\<turnstile>\<^sub>c" 55)
+  where "double_turnstile_CL F G \<equiv> (F \<turnstile>\<^sub>c G) \<and> (G \<turnstile>\<^sub>c F) "
 
-(* Missing BBI_form induction proof*)
+subsection "LM axioms"
 
-section "Axioms and Rules"
-text \<open>Now we try to implement CL and LM Rules through lemmas. 
-Due to our definition we can cast our BBI_form to a HOL object.
-All the rules can be proved using basic HOL rules without setting up customise proof notepad
-The auto prover help us to prove all LM rules.However, it does not show sub goals of proofs which is 
-opposite to CL lemmas\<close>
+text \<open> LM axioms are defined using same techique from CL\<close>
 
-text \<open>The following axiom rules apply to both CL and LM\<close>
-lemma Ax : "evalF f \<Longrightarrow> evalF f"
-  by blast
+inductive turnstile_LM ::"BBI_form \<Rightarrow> BBI_form \<Rightarrow> bool" (infix "\<turnstile>\<^sub>l" 55)
+  where
+Ax : "F \<turnstile>\<^sub>l F"|
+Topl : "(F *\<^sub>B \<top>\<^sub>B) \<turnstile>\<^sub>l F"|
+Topr : "F \<turnstile>\<^sub>l (F *\<^sub>B \<top>\<^sub>B)"|
+imp : "F *\<^sub>B G \<turnstile>\<^sub>l H \<Longrightarrow> F \<turnstile>\<^sub>l (G \<rightarrow>\<^emph>\<^sub>B H)"|
+MP : "F \<turnstile>\<^sub>l G \<Longrightarrow> G \<turnstile>\<^sub>l H \<Longrightarrow> F \<turnstile>\<^sub>l H"|
+Assocl: "F *\<^sub>B (G *\<^sub>B H) \<turnstile>\<^sub>l (F *\<^sub>B G) *\<^sub>B H"|
+Assocr: "(F *\<^sub>B G) *\<^sub>B H \<turnstile>\<^sub>l F *\<^sub>B (G *\<^sub>B H)"|
+Comm : "(F *\<^sub>B G) \<turnstile>\<^sub>l (G *\<^sub>B F)"|
+ConjI : "F1  \<turnstile>\<^sub>l G1 \<Longrightarrow> F2  \<turnstile>\<^sub>l G2 \<Longrightarrow> (F1 *\<^sub>B F2) \<turnstile>\<^sub>l (G1 *\<^sub>B G2)"
 
-lemma MP : "\<lbrakk>evalF F \<Longrightarrow> evalF G; evalF G \<Longrightarrow> evalF H\<rbrakk> \<Longrightarrow> (evalF F \<Longrightarrow> evalF H)"
-  by blast
-
-subsection "Classical Logic Axioms and Rules"
-
-
-(* IL Rules*)
-lemma top: "evalF f \<Longrightarrow> True"
-  by auto
-
-lemma bot: "False \<Longrightarrow> evalF f"
-  by auto
-
-lemma disI :"\<lbrakk>evalF F; evalF G\<rbrakk> \<Longrightarrow> (evalF F \<or> evalF G)"
-  by blast
-
-lemma conI : "\<lbrakk>evalF F \<Longrightarrow> evalF G; evalF F \<Longrightarrow> evalF H\<rbrakk> \<Longrightarrow> evalF F \<Longrightarrow> evalF G \<and> evalF H" 
-  by blast
-
-lemma imp : "\<lbrakk>evalF F \<and> evalF G \<Longrightarrow> evalF H\<rbrakk> \<Longrightarrow> evalF F \<Longrightarrow> evalF G \<longrightarrow> evalF H"
-  by blast
-
-lemma conE1 : "\<lbrakk>evalF F \<and> evalF G\<rbrakk> \<Longrightarrow> evalF F"
-  by blast
-
-lemma conE2 : "\<lbrakk>evalF F \<and> evalF G\<rbrakk> \<Longrightarrow> evalF G"
-  by blast
-
-lemma disE : "\<lbrakk>evalF F \<Longrightarrow> evalF H; evalF G \<Longrightarrow> evalF H\<rbrakk> \<Longrightarrow> (evalF F \<or> evalF G \<Longrightarrow> evalF H)"
-  by blast
+definition double_turnstile_LM :: "BBI_form \<Rightarrow> BBI_form \<Rightarrow> bool" (infix "\<stileturn>\<turnstile>\<^sub>l" 55)
+  where "double_turnstile_LM F G \<equiv> (F \<turnstile>\<^sub>l G) \<and> (G \<turnstile>\<^sub>l F)"
 
 
-(* CL rules*)
-lemma not : "\<lbrakk>\<not>(evalF F) \<Longrightarrow> evalF F ; evalF F \<Longrightarrow> \<not>(evalF F)\<rbrakk> \<Longrightarrow> False"
-  by blast
-
-lemma notnot : "\<lbrakk>\<not>\<not>(evalF F)\<rbrakk> \<Longrightarrow> evalF F"
-  by blast
-  
-subsection "Lambek Logic Axioms and Rules"
-
-(* Associative rule*)
-lemma assoc : "evalF (F  *\<^sub>B (G *\<^sub>B H)) \<longleftrightarrow> evalF ((F *\<^sub>B G)*\<^sub>B H)"
-  by auto
-
-lemma Mtruth : "evalF (F *\<^sub>B \<top>\<^sub>B ) \<longleftrightarrow> evalF F"
-  by auto
-
-(* Commutative rule*)
-lemma comm : "evalF (F *\<^sub>B G) \<Longrightarrow> evalF(G *\<^sub>B F)"
-  by auto
-
-lemma MconI : "\<lbrakk>evalF F\<^sub>1 \<Longrightarrow> evalF G\<^sub>1 ;evalF F\<^sub>2 \<Longrightarrow> evalF G\<^sub>2\<rbrakk> \<Longrightarrow> (evalF (F\<^sub>1 *\<^sub>B F\<^sub>2) \<Longrightarrow> evalF (G\<^sub>1 *\<^sub>B G\<^sub>2))"
-  by auto
-
-lemma Mimp : "\<lbrakk>evalF (F *\<^sub>B G) \<Longrightarrow> evalF H\<rbrakk> \<Longrightarrow> (evalF F \<Longrightarrow> evalF(G \<rightarrow>*\<^sub>B H))"
-  by auto
-
-
- 
+end
---
+++ Structure.thy
@@ -0,0 +1,36 @@
+(*  Title:      structure.thy
+    Author:     Tony Siu
+*)
+
+theory Structure
+  imports Main BBI
+begin
+
+text \<open> We imported BBI formula definition in this thy file. And now we will be defining 
+Structures for sequents and consecution\<close>
+
+(* Substructure occureences in a structure X are classified as either positive or negative*)
+datatype sign = positive | negative
+
+(* With previous syntatic definition of BBI formula, we now defining a structure *)
+datatype Structure =
+(* A BBI formula is an atomic Structure *)
+  formula  BBI_form
+(* Define addictive Structural connectives *)
+| AddNil                            ("\<emptyset>")            
+| Sharp Structure                 ("\<sharp> _" 100)
+| Semicolon Structure Structure   (infix ";" 101)
+| RightArrow Structure Structure  (infix "\<Rightarrow>\<^sub>s" 101)
+(* Define multiplicative Structural Connectives *)
+| MultiNil                        ("\<oslash>")     
+| Flat Structure                  ("\<flat> _" 100)
+| Comma Structure Structure       (infix "," 101)
+| DotArrow Structure Structure    (infix "\<comment>\<circ>" 101)
+
+(* Following code is an idea to define what is valid for structures in loging \<LL>*)
+type_synonym valid = "Structure \<Rightarrow> Structure \<Rightarrow> bool" ( "\<turnstile>")
+
+(* Need to define Antecedent and Consequent meaning using sign datatype*)
+datatype consecute = Antecedent Structure | Consequent Structure
+
+end
